c923f249f31ceb763533dabb8053fef673ae8533
matibieniek <mativlogerbusiness@gmail.com>
2024-08-28 18:40:56 +0200

    Add first message

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import socket
 
 HOST = "0.0.0.0"
 PORT = 10000
+ENCODING = 'utf-8'
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST, PORT))
@@ -11,3 +12,8 @@ server.listen(5)
 while True:
     client_socket, adress = server.accept()
     print(f"Connected to {adress}")
+    message = client_socket.recv(1024).decode(ENCODING)
+    print(f"Cilent said: {message}")
+    client_socket.send("Hi my nigga").encode(ENCODING)
+    client_socket.close()
+    print(f"Connection to {adress} closed")
